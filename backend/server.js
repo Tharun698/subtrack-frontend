d6ea5bb0377dad9695ef8b3783e28e3a301afab2
Tharun698 <tharun3839@gmail.com>
2026-04-07 17:08:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -28,35 +27,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
-=======
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const app = express();
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Test route
-app.get("/", (req, res) => {
-  res.send("✅ Backend is running successfully 🚀");
-});
-
-// MongoDB connection
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch((err) => console.log("❌ DB Error:", err));
-
-// Start server
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
->>>>>>> 35f08eff3d2c91024c3707fd280ae7f087963f84
 });
